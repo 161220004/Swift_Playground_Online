5a1e1cb16434170de10aeba2ef5d49a5b3a35a88
161220004 <2904428882@qq.com>
2020-02-01 11:16:07 +0800

    前端界面显示 (debug)
    debug: Html中文件路径(href, src)少写开头“/”引发的血案

--- Vapor/SPO/Sources/App/routes.swift
+++ Vapor/SPO/Sources/App/routes.swift
@@ -5,12 +5,6 @@ public func routes(_ router: Router) throws {
     
     try router.register(collection: HelloController())
     
-//    try router.register(collection: PuzzleController())
-    
-    
-    
-    router.get("spo") { req -> Future<View> in
-        return try req.view().render("spo_p0")
-    }
+    try router.register(collection: PuzzleController())
     
 }
