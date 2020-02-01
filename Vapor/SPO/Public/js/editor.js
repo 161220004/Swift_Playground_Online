5cbb27fab0e0499369c4a00a7aa43cb86cfb3306
161220004 <2904428882@qq.com>
2020-02-01 20:59:50 +0800

    实现前后端简单传值

--- Vapor/SPO/Public/js/editor.js
+++ Vapor/SPO/Public/js/editor.js
@@ -1,25 +1,41 @@
-var editor = ace.edit("pz_editor");
-// 设置代码编辑器主题套装
-editor.setTheme("ace/theme/idle_fingers");
-// 设置当前编辑器支持的语言（似乎一次只能选一种语言，我暂时未找到一次支持多语言的写法）
-editor.getSession().setMode('ace/mode/swift');
-// 不使用检查者（解决下面的bug我才使用的这个）
-editor.getSession().setUseWorker(false);
-// 设置光标所在的激活行是否高亮
-editor.setHighlightActiveLine(true);
-// 设置代码的字体大小
-editor.setFontSize(14);
-// 设置是否开启代码折叠效果
-editor.getSession().setUseWrapMode(true);
-editor.getSession().setUseSoftTabs(true);
-// 开启代码自动提示补全
-editor.setOptions({
-  enableBasicAutocompletion: true,
-  enableSnippets: true,
-  enableLiveAutocompletion: true
-});
-// 监听编辑器内容变化
-editor.getSession().on('change', function(e) {
-  // 获取编辑区的代码内容
-  var data = editor.getValue();
-});
+(function($){
+  $(function(){
+
+    // 编辑器相关设置：
+    var editor = ace.edit("pz_editor");
+    // 主题风格
+    editor.setTheme("ace/theme/tomorrow_night_eighties");
+    // 当前语言Swift
+    editor.getSession().setMode('ace/mode/swift');
+    // 光标行高亮
+    editor.setHighlightActiveLine(true);
+    // 设置字体大小
+    editor.setFontSize(14);
+    // 开启代码折叠
+    editor.getSession().setUseWrapMode(true);
+    editor.getSession().setUseSoftTabs(true);
+    // 开启代码自动补全
+    editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true
+    });
+
+    // “Run”按钮点击事件
+    $("#run_code").click(function(){
+      var code = Object();
+      code.lines = editor.getValue();
+      alert("Get Value: \n" + $("#test_live_view").html());
+      $.post("/spo/p0/code", JSON.stringify(code), function(data){
+        // Get放在Post内，确保编译运行结束后才从后端取结果
+        $.get("/spo/p0/result", function(data){
+          $('.result').html(data);
+          alert("Get Result: \n" + JSON.stringify(data));
+          // Test: 更改LiveView区内容为后端返回的data
+          $("#test_live_view").html(JSON.stringify(data))
+        })
+      })
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- Vapor/SPO/Sources/App/Controllers/PuzzleController.swift
+++ Vapor/SPO/Sources/App/Controllers/PuzzleController.swift
@@ -6,6 +6,21 @@
 //
 
 import Vapor
+import HTTP
+
+struct Code: Codable, Content {
+    var lines: String?
+}
+
+struct Pace: Content {
+    var dx: Int
+    var dy: Int
+}
+struct Actions: Content {
+    var isLegal: Bool
+    var isRight: Bool
+    var steps: [Pace]
+}
 
 final class PuzzleController: RouteCollection {
     
@@ -16,6 +31,10 @@ final class PuzzleController: RouteCollection {
             group.get("list", use: getPuzzleList)
             
             group.get("p0", use: getPuzzle0)
+            
+            group.post("p0", "code", use: postCode)
+            
+            group.get("p0", "result", use: getResult)
             
         }
     }
@@ -31,6 +50,24 @@ extension PuzzleController {
     /// Get: one puzzle
     func getPuzzle0(_ req: Request) throws -> Future<View> {
         return try req.view().render("spo_p0")
+    }
+    
+    /// Post: User Code
+    func postCode(_ req: Request) throws -> Future<HTTPStatus> {
+        if let codeJson = req.http.body.data {
+            let codeObj = try JSONDecoder().decode(Code.self, from: String(data: codeJson, encoding:.utf8)!)
+            let code = codeObj.lines
+            // TODO: 编译运行
+            sleep(5)
+            print("I Am Awaked Now")
+        }
+        return try req.content.decode(Code.self).transform(to: HTTPStatus.created)
+    }
+    
+    /// Get: Result
+    func getResult(_ req: Request) throws -> Actions {
+        let actions: Actions = Actions(isLegal: true, isRight: true, steps: [Pace(dx: 0, dy: 0)])
+        return actions
     }
     
 }
