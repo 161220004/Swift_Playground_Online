f9aadbebd16da92d0d366924941a2a7ed279541e
161220004 <2904428882@qq.com>
2020-05-05 13:43:43 +0800

    优化Puzzle2-1：强制写函数

--- Vapor/Public/js/Ace/AutoComplete4.js
+++ Vapor/Public/js/Ace/AutoComplete4.js
@@ -3,5 +3,6 @@ setCompleteData([
   {meta: "向前走", caption: "goForward()", value: "goForward()", score:1000},
   {meta: "收集宝石", caption: "collect()", value: "collect()", score:1000},
   {meta: "向左转", caption: "turnLeft()", value: "turnLeft()", score:1000},
+  {meta: "向右转", caption: "turnRight()", value: "turnRight()", score:1000},
   {meta: "切换砖块", caption: "switchIt()", value: "switchIt()", score:1000}
 ]);
